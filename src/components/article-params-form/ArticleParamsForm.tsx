4843a05fffd14d3502200d5c6d4e2ab327413f7d
Roman Ergin <romanergin@yandex.ru>
2025-09-15 13:40:45 +0500

    initial commit

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,138 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import {
+	ArticleStateType,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	isOpen: boolean;
+	onToggle: () => void;
+	formSettings: ArticleStateType;
+	onFormSettingsChange: (settings: ArticleStateType) => void;
+	onApply: () => void;
+	onReset: () => void;
+}
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onToggle,
+	formSettings,
+	onFormSettingsChange,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const handleFontFamilyChange = (option: (typeof fontFamilyOptions)[0]) => {
+		console.log('Изменен шрифт:', option);
+		onFormSettingsChange({ ...formSettings, fontFamilyOption: option });
+	};
+
+	const handleFontColorChange = (option: (typeof fontColors)[0]) => {
+		console.log('Изменен цвет текста:', option);
+		onFormSettingsChange({ ...formSettings, fontColor: option });
+	};
+
+	const handleBackgroundColorChange = (
+		option: (typeof backgroundColors)[0]
+	) => {
+		console.log('Изменен цвет фона:', option);
+		onFormSettingsChange({ ...formSettings, backgroundColor: option });
+	};
+
+	const handleContentWidthChange = (option: (typeof contentWidthArr)[0]) => {
+		console.log('Изменена ширина контента:', option);
+		onFormSettingsChange({ ...formSettings, contentWidth: option });
+	};
+
+	const handleFontSizeChange = (option: (typeof fontSizeOptions)[0]) => {
+		console.log('Изменен размер шрифта:', option);
+		onFormSettingsChange({ ...formSettings, fontSizeOption: option });
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<h1 className={styles.title}>ЗАДАЙТЕ ПАРАМЕТРЫ</h1>
+
+					<Select
+						title='ШРИФТ'
+						selected={formSettings.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={handleFontFamilyChange}
+					/>
+
+					<Separator />
+
+					<Select
+						title='ЦВЕТ ШРИФТА'
+						selected={formSettings.fontColor}
+						options={fontColors}
+						onChange={handleFontColorChange}
+					/>
+
+					<Separator />
+
+					<Select
+						title='ЦВЕТ ФОНА'
+						selected={formSettings.backgroundColor}
+						options={backgroundColors}
+						onChange={handleBackgroundColorChange}
+					/>
+
+					<Separator />
+
+					<Select
+						title='ШИРИНА КОНТЕНТА'
+						selected={formSettings.contentWidth}
+						options={contentWidthArr}
+						onChange={handleContentWidthChange}
+					/>
+
+					<Separator />
+
+					<div className={styles.fontSizeGroup}>
+						<div className={styles.fontSizeLabel}>РАЗМЕР ШРИФТА</div>
+						<div className={styles.fontSizeButtons}>
+							{fontSizeOptions.map((option) => (
+								<button
+									key={option.value}
+									type='button'
+									className={clsx(styles.fontSizeButton, {
+										[styles.fontSizeButton_active]:
+											formSettings.fontSizeOption.value === option.value,
+									})}
+									onClick={() => handleFontSizeChange(option)}>
+									{option.title}
+								</button>
+							))}
+						</div>
+					</div>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='СБРОСИТЬ'
+							htmlType='button'
+							type='clear'
+							onClick={onReset}
+						/>
+						<Button
+							title='ПРИМЕНИТЬ'
+							htmlType='button'
+							type='apply'
+							onClick={onApply}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,64 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	// Текущие примененные настройки
+	const [appliedSettings, setAppliedSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+	// Настройки в форме (могут отличаться от примененных)
+	const [formSettings, setFormSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+	// Состояние сайдбара
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	const handleApplySettings = () => {
+		console.log('Применяем настройки:', formSettings);
+		setAppliedSettings(formSettings);
+		setIsSidebarOpen(false);
+	};
+
+	const handleResetSettings = () => {
+		console.log('Сбрасываем настройки на дефолтные:', defaultArticleState);
+		setFormSettings(defaultArticleState);
+		setAppliedSettings(defaultArticleState);
+		setIsSidebarOpen(false);
+	};
+
+	const handleSidebarToggle = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	};
+
+	// Отслеживаем изменения примененных настроек
+	useEffect(() => {
+		console.log('Примененные настройки обновлены:', appliedSettings);
+		console.log('CSS-переменные:', {
+			'--font-family': appliedSettings.fontFamilyOption.value,
+			'--font-size': appliedSettings.fontSizeOption.value,
+			'--font-color': appliedSettings.fontColor.value,
+			'--container-width': appliedSettings.contentWidth.value,
+			'--bg-color': appliedSettings.backgroundColor.value,
+		});
+	}, [appliedSettings]);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedSettings.fontFamilyOption.value,
+					'--font-size': appliedSettings.fontSizeOption.value,
+					'--font-color': appliedSettings.fontColor.value,
+					'--container-width': appliedSettings.contentWidth.value,
+					'--bg-color': appliedSettings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isSidebarOpen}
+				onToggle={handleSidebarToggle}
+				formSettings={formSettings}
+				onFormSettingsChange={setFormSettings}
+				onApply={handleApplySettings}
+				onReset={handleResetSettings}
+			/>
 			<Article />
 		</main>
 	);
